061371805dbde305fb5c9236c3ae60e5ecd1a1e6
ash <menicce4@gmail.com>
2024-12-02 13:24:43 +0000

    added ticket MVP 1

---
+++ ticket1.js
@@ -0,0 +1,6 @@
+//a JavaScript function `convertGBPtoEUR` that takes in a specified amount in £, 
+//and converts it to € using a given exchange rate. 
+//The function should return the converted amount.
+function convertGBPtoEU (GBP){
+    return GBP * 1.21;
+}
