e0ea3d700a64475bf16cb59d379a5710e4f4553d
pepland <136370802+pepland@users.noreply.github.com>
2024-12-02 13:31:25 +0000

    Completed ticket2/MVP2

---
+++ ticket2.js
@@ -0,0 +1,7 @@
+//Write another function convertGBPtoUSD 
+//that takes in a specified amount in £, 
+//and converts it to $ using a given exchange rate. 
+
+function convertGBPtoUSD (GBP) {
+    return GBP * 1.27;
+}
